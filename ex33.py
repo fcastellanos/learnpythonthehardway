78996a4c71e926d9c442b91e751e2ec7d49bc8f4
Fernando Castellanos <fernando.castellanos@gmail.com>
2014-03-31 23:53:06 -0700

    Update exercise 33 applying some study drills

--- ex33.py
+++ ex33.py
@@ -1,17 +1,17 @@
-# moved the while logic to a method as the study drill suggested
-def load_numbers_list(limit):
+# moved the while logic to a method as the study drills suggested
+def load_numbers_list(limit, increments_by):
   i = 0
   while i < limit:
     print "At the top i is %d" % i
     numbers.append(i)
 
-    i = i + 1
+    i = i + increments_by
     print "Numbers now: ", numbers
     print "At the bottom i is %d" % i
 
 numbers = []
 
-load_numbers_list(6)
+load_numbers_list(6, 1)
 
 print "The numbers: "
 
