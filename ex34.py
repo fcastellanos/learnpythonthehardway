37d3ec95d46f4d6e5a549ddcc2975d4b5c2adada
Fernando Castellanos <fernando.castellanos@gmail.com>
2014-04-01 22:29:11 -0700

    Add exercise 34

---
+++ ex34.py
@@ -0,0 +1,15 @@
+print "The Animals:"
+
+animals = ['bear', 'python', 'peacock', 'kangaroo', 'whale', 'platypus']
+
+print animals
+
+print "\n"
+print "1.- The animal at 1 is %s" % animals[1]
+print "2.- The third (3rd) animal is at 2 and is a %s" % animals[2]
+print "3.- The first (1st) animal is at 0 and is a %s" % animals[0]
+print "4.- The animal at 3 is %s" % animals[3]
+print "5.- The fifth (5th) animal is at 4 and is a %s" % animals[4]
+print "6.- The animal at 2 is %s" % animals[2]
+print "7.- The sixth (6th) animal is at 5 and is a %s" % animals[5]
+print "8.- The animal at 4 is %s" % animals[4]
